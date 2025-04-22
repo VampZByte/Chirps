9b1dfddbc957bf8d44897a7d041035a44c23be09
VampZByte <vampzbyte@gmail.com>
2025-04-22 15:39:38 +0800

    New commit

---
+++ database/migrations/2025_03_31_083714_create_customers_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->customer_id();
+            $table->customer_fname();
+            $table->customer_lname();
+            $table->age();
+            $table->phone();
+            $table->address();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customers');
+    }
+};
