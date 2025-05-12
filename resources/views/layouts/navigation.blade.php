90eb7bd25b42980bbdfe98f8a38556a10dff0970
Alden Kenshin <aldenkenshin6@gmail.com>
2025-05-13 01:50:35 +0800

    boii

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <!-- logo ninyo --><x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                        <img src="{{ asset('image/logo.jpg') }}" alt="logo" style="width: 50px; height: auto;">
                     </a>
                 </div>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -58,6 +58,7 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
+            <img src="{{ asset('image/logo.jpg') }}" alt="logo" style="width: 50px; height: auto;">
             <h1>Car Rentals</h1>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
